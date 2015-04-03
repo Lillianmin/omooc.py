84a42ba28f3fdac99fb8643d9ea5fdd371833c49
mll <mll@mll.zl>
2015-04-03 18:25:54 +0800

    add test_file.py

---
+++ src/iippy-2/test_file.py
@@ -0,0 +1,34 @@
+#Test file open, save, write, read
+
+import tkFileDialog
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+#init
+shape_list=[]
+
+#function must def before it used
+def save_draw():
+    save_file = tkFileDialog.asksaveasfile(mode='w')
+    for index in range(5):
+        save_file.write(str(index)+"\n")
+    save_file.flush()
+    save_file.close()
+
+def open_draw():
+    global shape_list
+    end = False
+    open_file = tkFileDialog.askopenfile(mode='r')
+    while not end:
+        shape_str = open_file.readline(1024)
+        print shape_str
+        if shape_str == "":
+            end = True
+        else:
+            shape_list.append(shape_str)
+    open_file.close()
+
+frame = simplegui.create_frame("test", 1000, 600)
+frame.add_button("Save", save_draw)
+frame.add_button("Open", open_draw)
+frame.start()
+
